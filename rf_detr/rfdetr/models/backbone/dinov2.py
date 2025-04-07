93749944645883819781f27c710bb934d1a1b6c7
allankouidri <allan.kouidri@gmail.com>
2025-04-07 10:08:45 +0200

    Edit Dinov2 path

--- rf_detr/rfdetr/models/backbone/dinov2.py
+++ rf_detr/rfdetr/models/backbone/dinov2.py
@@ -15,22 +15,25 @@ size_to_width = {
     "large": 1024,
 }
 
+
 class DinoV2(nn.Module):
     def __init__(self, shape=(640, 640), out_feature_indexes=[2, 4, 5, 9], size="base", use_registers=True, use_windowed_attn=True, gradient_checkpointing=False):
         super().__init__()
 
         name = f"facebook/dinov2-with-registers-{size}" if use_registers else f"facebook/dinov2-{size}"
-
+        # Edit cache dir to be weights dir
+        weights_dir = os.path.join(
+            os.path.dirname(__file__), "..", "..", "..", "..", "weights")
         self.shape = shape
-        
+
         # Create the encoder
-        
         if not use_windowed_attn:
             assert not gradient_checkpointing, "Gradient checkpointing is not supported for non-windowed attention"
             self.encoder = AutoBackbone.from_pretrained(
                 name,
                 out_features=[f"stage{i}" for i in out_feature_indexes],
                 return_dict=False,
+                cache_dir=weights_dir,
             )
         else:
             window_block_indexes = set(range(out_feature_indexes[-1] + 1))
@@ -41,6 +44,7 @@ class DinoV2(nn.Module):
                 name,
                 out_features=[f"stage{i}" for i in out_feature_indexes],
                 return_dict=False,
+                cache_dir=weights_dir
             )
             if use_registers:
                 windowed_dino_config = WindowedDinov2WithRegistersConfig(
@@ -60,10 +64,11 @@ class DinoV2(nn.Module):
             self.encoder = WindowedDinov2WithRegistersBackbone.from_pretrained(
                 name,
                 config=windowed_dino_config,
+                cache_dir=weights_dir
             )
 
-
-        self._out_feature_channels = [size_to_width[size]] * len(out_feature_indexes)
+        self._out_feature_channels = [
+            size_to_width[size]] * len(out_feature_indexes)
         self._export = False
 
     def export(self):
@@ -71,6 +76,7 @@ class DinoV2(nn.Module):
             return
         self._export = True
         shape = self.shape
+
         def make_new_interpolated_pos_encoding(
             position_embeddings, patch_size, height, width
         ):
@@ -85,7 +91,8 @@ class DinoV2(nn.Module):
 
             # Reshape and permute
             patch_pos_embed = patch_pos_embed.reshape(
-                1, int(math.sqrt(num_positions)), int(math.sqrt(num_positions)), dim
+                1, int(math.sqrt(num_positions)), int(
+                    math.sqrt(num_positions)), dim
             )
             patch_pos_embed = patch_pos_embed.permute(0, 3, 1, 2)
 
@@ -99,7 +106,8 @@ class DinoV2(nn.Module):
             )
 
             # Reshape back
-            patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).reshape(1, -1, dim)
+            patch_pos_embed = patch_pos_embed.permute(
+                0, 2, 3, 1).reshape(1, -1, dim)
             return torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed), dim=1)
 
         # If the shape of self.encoder.embeddings.position_embeddings
@@ -113,6 +121,7 @@ class DinoV2(nn.Module):
             )
         # Create a new Parameter with the new size
         old_interpolate_pos_encoding = self.encoder.embeddings.interpolate_pos_encoding
+
         def new_interpolate_pos_encoding(self_mod, embeddings, height, width):
             num_patches = embeddings.shape[1] - 1
             num_positions = self_mod.position_embeddings.shape[1] - 1
@@ -120,16 +129,19 @@ class DinoV2(nn.Module):
                 return self_mod.position_embeddings
             return old_interpolate_pos_encoding(embeddings, height, width)
 
-        self.encoder.embeddings.position_embeddings = nn.Parameter(new_positions)
+        self.encoder.embeddings.position_embeddings = nn.Parameter(
+            new_positions)
         self.encoder.embeddings.interpolate_pos_encoding = types.MethodType(
-            new_interpolate_pos_encoding, 
+            new_interpolate_pos_encoding,
             self.encoder.embeddings
         )
 
     def forward(self, x):
-        assert x.shape[2] % 14 == 0 and x.shape[3] % 14 == 0, f"Dinov2 requires input shape to be divisible by 14, but got {x.shape}"
+        assert x.shape[2] % 14 == 0 and x.shape[
+            3] % 14 == 0, f"Dinov2 requires input shape to be divisible by 14, but got {x.shape}"
         x = self.encoder(x)
         return list(x[0])
+
 
 if __name__ == "__main__":
     model = DinoV2()
